a22473534952d43522d9ea292a21e3d151e129d5
glebanya13 <65718268+glebanya13@users.noreply.github.com>
2022-11-07 13:37:05 +0300

    init 3

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.db.models import Q
 from django.views.generic import FormView, ListView
 
@@ -9,6 +10,19 @@ from .forms import HotelForm
 class HomePageView(FormView):
     form_class = HotelForm
     template_name = 'index.html'
+    success_url = '/hotels'
+    failed_url = '/home'
+
+    def form_valid(self, form):
+        name = form.cleaned_data.get("name")
+        country = form.cleaned_data.get("country")
+        
+        obj =  Hotel.objects.filter(
+            Q(name__icontains=name) | Q(country__icontains=country)
+        )
+
+        return redirect(reverse_lazy('hotels'), hotels=obj)
+    
 
 class HotelsPageView(ListView):
     model = Hotel
